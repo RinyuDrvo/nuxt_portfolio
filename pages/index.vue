b14fe40e49f59d7faf5e28f6f3badf2ab96971af
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-05-26 09:48:41 +0900

    make index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,24 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Hello
-      </h1>
-      <h2 class="subtitle">
-        My pioneering Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="page-index">
+    <section class="about">
+      <h2>夏目漱石</h2>
+      <div class="article-body">
+        <img src="~/assets/images/owl.png" alt="souseki" />
+        <p>日本の小説家日本の小説家日本の小説家</p>
       </div>
-    </div>
+    </section>
+
+    <section>
+      <h2>代表作</h2>
+      <div class="article-body">
+        <p>吾輩は猫であるがデビュー作</p>
+        <ul>
+          <li>吾輩は猫ある</li>
+          <li>吾輩は猫ある</li>
+          <li>吾輩は猫ある</li>
+          <li>吾輩は猫ある</li>
+        </ul>
+      </div>
+    </section>
   </div>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
