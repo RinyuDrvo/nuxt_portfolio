0fb0491097c4d62c418317adf492cf32d897b480
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-05-27 21:26:24 +0900

    use article body

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,21 +2,31 @@
   <div class="page-about">
     <section class="about">
       <h2>初めまして</h2>
-      <div class="article-body">
+      <ArticleBody>
         <p>夏目漱石夏目漱石夏目漱石夏目漱石夏目漱石夏目漱石夏目漱石夏目漱石</p>
-      </div>
+      </ArticleBody>
     </section>
     <section class="history">
       <h2>経歴</h2>
-      <div class="article-body">
+      <ArticleBody>
         <h3>幼少期</h3>
         <p>どこに生まれてどこで育った</p>
         <h3>イギリス留学</h3>
         <p>英語教師英語教師英語教師英語教師英語教師英語教師</p>
-      </div>
+      </ArticleBody>
     </section>
   </div>
 </template>
+
+<script>
+import ArticleBody from '~/components/common/ArticleBody'
+
+export default {
+  components: {
+    ArticleBody
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .about,
@@ -35,41 +45,6 @@
       margin-left: 10px;
       background-color: $color-black;
     }
-  }
-}
-
-.article-body {
-  h3 {
-    font-size: 20px;
-    margin: 30px 0 20px;
-  }
-
-  p {
-    line-height: 1.75;
-
-    + p {
-      margin-top: 20px;
-    }
-
-    a {
-      color: $color-blue;
-    }
-  }
-
-  ul {
-    list-style: circle;
-    margin-top: 20px;
-    margin-left: 20px;
-
-    li {
-      &:nth-of-type(n + 2) {
-        margin-top: 0.5em;
-      }
-    }
-  }
-
-  img {
-    max-width: 100%;
   }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,15 @@
   <div class="page-index">
     <section class="about">
       <h2>夏目漱石</h2>
-      <div class="article-body">
+      <ArticleBody>
         <img src="~/assets/images/owl.png" alt="souseki" />
         <p>日本の小説家日本の小説家日本の小説家</p>
-      </div>
+      </ArticleBody>
     </section>
 
     <section>
       <h2>代表作</h2>
-      <div class="article-body">
+      <ArticleBody>
         <p>吾輩は猫であるがデビュー作</p>
         <ul>
           <li>吾輩は猫ある</li>
@@ -18,10 +18,20 @@
           <li>吾輩は猫ある</li>
           <li>吾輩は猫ある</li>
         </ul>
-      </div>
+      </ArticleBody>
     </section>
   </div>
 </template>
+
+<script>
+import ArticleBody from '~/components/common/ArticleBody'
+
+export default {
+  components: {
+    ArticleBody
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .about,
@@ -40,39 +50,6 @@
       margin-left: 10px;
       background-color: $color-black;
     }
-  }
-}
-
-.article-body {
-  h3 {
-    font-size: 20px;
-    margin: 30px 0 20px;
-  }
-
-  p {
-    line-height: 1.75;
-    + p {
-      margin-top: 20px;
-    }
-
-    a {
-      color: $color-blue;
-    }
-  }
-
-  ul {
-    list-style: circle;
-    margin-top: 20px;
-    margin-left: 20px;
-
-    li {
-      &:nth-of-type(n + 2) {
-        margin-top: 0.5em;
-      }
-    }
-  }
-  img {
-    max-width: 100%;
   }
 }
 </style>
