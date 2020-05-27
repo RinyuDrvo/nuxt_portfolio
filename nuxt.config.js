9105b77284b05b038c805dd0155958d16556a330
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-05-27 09:53:16 +0900

    make css

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,10 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: [
+    'reset-css',
+    '~/assets/scss/style.scss'
+  ],
   /*
    ** Plugins to load before mounting the App
    */
